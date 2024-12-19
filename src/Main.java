52ea7f0cd807cb9d7b123a8c7bd94f1547dae020
MSI1 <aryansharma1126@gmail.com>
2024-12-19 09:42:36 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.print("Second commit");
+        System.out.print("heck ya commit");
+        System.out.print("how am i commit");
+        System.out.print("playfull commit");
+
     }
 }
